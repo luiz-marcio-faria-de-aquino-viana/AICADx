6e162132fecd0cb9ace2b6122b94a361d0d6e40c
Luiz Marcio Faria de Aquino Viana, Pos-D.Sc. <151483374+luiz-marcio-faria-de-aquino-viana@users.noreply.github.com>
2025-07-09 01:59:53 -0300

    AICADx-202503050104-lmarcio
    /*
     * Copyright(C) TLMV Consultoria e Sistemas EIRELI. Todos os direitos reservados.
     *
     * AppDefs.java
     * Autor: 
     *   Luiz Marcio Faria de Aquino Viana, Pos-D.Sc. - Engenheiro, 23/01/2025
     *   Unidade: Universidade do Estado do Rio de Janeiro
     *   Curso: Engenharia Eletrica, Enfase em Engenharia de Sistemas e Computação
     *   Unico Socio e Administrador da Empresa - Desde: 02/08/2000
     *
     * Revisoes: ...
     *
     */

---
+++ AICADx-202503050104-lmarcio/src/br/com/tlmv/aicadxapp/res/strings/R.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright(C) TLMV Consultoria e Sistemas EIRELI. Todos os direitos reservados.
+ *
+ * R.java
+ * Autor: 
+ *   Luiz Marcio Faria de Aquino Viana, Pos-D.Sc. - Engenheiro, 26/01/2025
+ *   Unidade: Universidade do Estado do Rio de Janeiro
+ *   Curso: Engenharia Eletrica, Enfase em Engenharia de Sistemas e Computação
+ *   Unico Socio e Administrador da Empresa - Desde: 02/08/2000
+ *
+ * Revisoes: ...
+ *
+ */
+
+package br.com.tlmv.aicadxapp.res.strings;
+
+public interface R 
+{
+//Public
+	
+	public String getString(String name);
+	
+	public String getMonthFullName(int month);
+	
+	public String getMonthAbrev(int month);
+	
+}
